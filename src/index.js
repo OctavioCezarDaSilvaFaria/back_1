57eb69dcd27150dec9b0a5f454aa16ca864c6a24
OctavioCezarDaSilvaFaria <tavinhojabuca@gmail.com>
2024-09-19 13:53:23 -0300

    customer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import productRoutes from './routes/productRoutes.js';
+import customerRoutes from './routes/customerRoutes.js';
 
 const app = express();
 app.use(express.json());
 
-app.use(productRoutes);
+// app.use(productRoutes);
+
+app.use(customerRoutes);
 
 app.listen(3000, () => {
   console.log(`Servidor rodando na porta 3000`);
